70f3effb463e5e8bbcf5de012d09e0c7bd151e25
Alona <mykyshka2013@gmail.com>
2024-08-11 21:30:04 +0300

    header

---
+++ src/js/mobile.js
@@ -0,0 +1,35 @@
+(() => {
+    const mobileMenu = document.querySelector('.js-menu-container');
+    const openMenuBtn = document.querySelector('.js-open-menu');
+    const closeMenuBtn = document.querySelector('.js-close-menu');
+    const bodyScrollLock = {
+      disableBodyScroll() {
+        document.body.style.overflow = 'hidden';
+      },
+      enableBodyScroll() {
+        document.body.style.overflow = 'auto';
+      },
+    };
+  
+    const toggleMenu = () => {
+      const isMenuOpen =
+        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenu.classList.toggle('is-open');
+  
+      const scrollLockMethod = !isMenuOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod]();
+    };
+  
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+  
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll();
+    });
+  })();
